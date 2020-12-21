832a8b1a94390295f8676eae15b491436d90315c
Beka Gelashvili <beka.gelash@gmail.com>
2020-12-21 16:31:27 +0400

    remove api key

--- MyMovies/Helpers/Network.swift
+++ MyMovies/Helpers/Network.swift
@@ -10,7 +10,7 @@ import UIKit
 class Network {
 
     static let moviesApirUrl = "https://api.themoviedb.org/3"
-    static let moviesApiKey = "76f96247f4450927937cf79fab4d97e9"
+//    static let moviesApiKey = ""
 
     static func moviePoster(_ posterPath: String) -> String {
         return "https://image.tmdb.org/t/p/original\(posterPath)"
